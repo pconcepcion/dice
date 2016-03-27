81cfda9d015f68c1216c5a244d1df655b2e9fc13
Pablo de la Concepcion <pconcepcion@gmail.com>
2016-03-27 14:09:29 +0200

    Renamed dice_expresions.go to dice_expressions.go, added parse modifiers function, added quite some tests to the test table

---
+++ dice_expressions.go
@@ -0,0 +1,121 @@
+// Package rpg provides tools to develop rpg games
+package rpg
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const (
+	wholeExpression = iota
+	numDicesPosition
+	numSidesPosition
+	modifierExpression
+)
+
+// SimpleDiceExpression represents a dice expression with just one type of dice
+// dice expresions are based on the ones in RPtools ( http://lmwcs.com/rptools/wiki/Dice_Expressions )
+//
+type SimpleDiceExpression struct {
+	expressionText string // Text that represents the dice expression
+	numDices       int    // number of dices
+	sides          int    // dice sides
+	modifier       int    // modifier to the result
+	drop           int    // number of lower results to drop
+	keep           int    // number of lower results to keep
+	explodeResult  int    // explode the result if greater than X
+	target         int    // count as success if greater than X
+	rerollL        int    // number of lower dices to reroll
+	rerollH        int    // number of high results to reroll
+	constant       int    // constant value
+}
+
+/**
+ * Parse a simple dice expresion and save the relevant information on the struct
+ */
+func (sde *SimpleDiceExpression) parse() ([]string, error) {
+	var err error
+	sde.expressionText = strings.TrimSpace(sde.expressionText)
+	// TODO: regular expresions should probably be compiled once at the initialization of the package
+	re := regexp.MustCompile(`(?P<numDices>\d*)d(?P<numSides>\d+)(?P<modifiers>.*)`)
+	constantRegexp := regexp.MustCompile(`^\d+$`)
+	// Check if it's a constant dice expression
+	if constantRegexp.MatchString(sde.expressionText) == true {
+		sde.constant, err = strconv.Atoi(sde.expressionText)
+		if err != nil {
+			return nil, err
+		}
+		return []string{sde.expressionText, "", ""}, nil
+	}
+	dices := re.FindStringSubmatch(strings.TrimSpace(sde.expressionText))
+	if dices == nil {
+		return nil, errors.New("Invalid dice expression")
+	}
+	// If it's not specified assume it's one dice
+	sde.numDices = 1
+	if dices[numDicesPosition] != "" {
+		sde.numDices, err = strconv.Atoi(dices[numDicesPosition])
+		if err != nil {
+			return nil, err
+		}
+	}
+	sde.sides, err = strconv.Atoi(dices[numSidesPosition])
+	if err != nil {
+		return nil, err
+	}
+	if dices[modifierExpression] != "" {
+		modifiers, err := sde.parseModifiers(dices[modifierExpression])
+		if err != nil {
+			return nil, err
+		}
+		dices = append(dices[:len(dices)-1], modifiers[1:]...)
+	}
+	return dices, nil
+}
+
+/**
+ * parse the modifiers part of the exprssion and store the values on the expression structure
+ */
+func (sde *SimpleDiceExpression) parseModifiers(modifierString string) ([]string, error) {
+	var err error
+	re := regexp.MustCompile(`(?P<modifier>[dekors])(?P<value>\d*)(?P<addSubstract>[+-]*)(?P<constant>\d*)`)
+	modifiers := re.FindStringSubmatch(modifierString)
+	fmt.Printf("modifiers %s %#v\n", modifierString, modifiers)
+	if modifiers == nil {
+		return nil, errors.New("Invalid dice expression")
+	}
+	switch modifierString[0] {
+	case 'k': // Keep
+		if modifiers[2] == "" {
+			return nil, errors.New("Invalid dice expression, missing number of dices to keep")
+		}
+		sde.keep, err = strconv.Atoi(modifiers[2])
+		if err != nil {
+			return nil, err
+		}
+	case 'r': // Rerroll
+		if modifiers[2] == "" {
+			return nil, errors.New("Invalid dice expression, missing number of dices to reroll")
+		}
+		sde.rerollL, err = strconv.Atoi(modifiers[2])
+		if err != nil {
+			return nil, err
+		}
+	case 's': // Keep
+		if modifiers[2] == "" {
+			return nil, errors.New("Invalid dice expression, missing success number")
+		}
+		sde.target, err = strconv.Atoi(modifiers[2])
+		if err != nil {
+			return nil, err
+		}
+	// case 'd': // Discard
+
+	default:
+		return nil, errors.New("Invalid dice modifier")
+	}
+	return modifiers, nil
+}
--- dice_expressions_test.go
+++ dice_expressions_test.go
@@ -14,18 +14,50 @@ func TestParse(t *testing.T) {
 		dices int
 		sides int
 	}{
-		{"d3", []string{"", "3"}, 1, 3},
+		// TODO: test with spaces and invalid dice expressions
+		// Constants
+		{"1", []string{"", ""}, 0, 0},
+		{"10", []string{"", ""}, 0, 0},
+		{"100", []string{"", ""}, 0, 0},
+		{"1000", []string{"", ""}, 0, 0},
+		{"4321", []string{"", ""}, 0, 0},
+		{" 1000", []string{"", ""}, 0, 0},
+		{"1000 ", []string{"", ""}, 0, 0},
+		{"10 00", []string{"", ""}, 0, 0},
+		{" 1000 ", []string{"", ""}, 0, 0},
+		// Basic dices
+		{"d2", []string{"", "2"}, 1, 2},
+		{"d4", []string{"", "4"}, 1, 4},
+		{"d6", []string{"", "6"}, 1, 6},
+		{"d8", []string{"", "8"}, 1, 8},
+		{"d10", []string{"", "10"}, 1, 10},
+		{"d12", []string{"", "12"}, 1, 12},
+		{"d20", []string{"", "20"}, 1, 20},
+		{"d100", []string{"", "100"}, 1, 100},
+		{"d200", []string{"", "200"}, 1, 200},
+		{"d1000", []string{"", "1000"}, 1, 1000},
+		// More complex expressions
 		{"3d3", []string{"3", "3"}, 3, 3},
-		{"3d3d3", []string{"3", "3", "d3"}, 3, 3},
-		{"3d4d5", []string{"3", "4", "d5"}, 3, 4},
-		{"3d4d5+2", []string{"3", "4", "d5+2"}, 3, 4},
+		//{"3d3d3", []string{"3", "3", "d3"}, 3, 3},
+		//{"3d4d5", []string{"3", "4", "d5"}, 3, 4},
+		//{"3d4d5+2", []string{"3", "4", "d5+2"}, 3, 4},
+		{"1d2", []string{"1", "2"}, 1, 2}, // rolls one two sides die and calculates the sum
+		//{"2d6d1", []string{"2", "6", "d1"}, 2, 6},   // rolls two six-sided dice, drops the lowest roll, and sums the total
+		{"3d6k3", []string{"3", "6", "k3"}, 3, 6},   // rolls thee six-sided dice, keeps the highest 3 rolls, and presents the total
+		{"4d8r2", []string{"4", "8", "r2"}, 4, 8},   // rolls four eight-sided dice, repeatedly rerolls any dice that are lower than 2 until all dice rolls are higher than or equal to 2, and then sums and presents the total
+		{"5d6s4", []string{"5", "6", "s4"}, 5, 6},   // rolls five six-sided dice, and counts any individual roll that exceeds four, presenting the number of "successes"
+		{"6d6e", []string{"6", "6", "e"}, 6, 6},     // rolls eight six-sided dice, and if either rolls a 6, it is rerolled and added to the total (this continues until neither die rolls a 6).
+		{"7d6es8", []string{"7", "6", "es8"}, 7, 6}, // will roll seven six-sided dice, explode any that roll their maximum, and then total the rolls that exceed 8
+		{"8d6o", []string{"8", "6", "o"}, 8, 6},     // rolls 5 six-sided dice, and explodes any that roll 6
 	}
+
 	var sde SimpleDiceExpression
 	for i, pts := range parseTestStrings {
 		sde.expressionText = pts.s
 		res, err := sde.parse()
+		//fmt.Printf("sde: %+v", sde)
 		if err != nil {
-			t.Errorf("%d) Failed to parse %s", i, sde.expressionText)
+			t.Errorf("%d) Failed to parse %s: %v", i, sde.expressionText, err)
 		} else {
 			for j, v := range pts.out {
 				if v != res[j+1] {
@@ -36,7 +68,7 @@ func TestParse(t *testing.T) {
 				t.Errorf("%d) Failed to parse %s: sides doesn't match %d != %d", i, sde.expressionText, sde.sides, pts.sides)
 			}
 			//t.Logf("Parsed %s\n", sde.expressionText)
-			fmt.Printf("%d) Parsed %s -> %v\n", i, sde.expressionText, res)
+			fmt.Printf("%d) Parsed %s -> %#v\n", i, sde.expressionText, res)
 		}
 	}
 }
