2d0262d22cd8b2c22f176d669a908af935887a7f
Pablo de la Concepcion <pconcepcion@gmail.com>
2016-04-23 12:54:50 +0200

    Improved test suite

--- dice_lexer_test.go
+++ dice_lexer_test.go
@@ -28,7 +28,6 @@ func TestLexer(t *testing.T) {
 		s   string
 		out []Token
 	}{
-		// TODO: do more test with spaces and invalid dice expressions
 		// Constants
 		{"1", []Token{Token{tokenNumber, "1"}, Token{tokenEOF, ""}}},
 		{"10", []Token{Token{tokenNumber, "10"}, Token{tokenEOF, ""}}},
@@ -60,7 +59,7 @@ func TestLexer(t *testing.T) {
 		{"7d6es8", []Token{Token{tokenNumber, "7"}, Token{tokenDice, "d"}, Token{tokenNumber, "6"}, Token{tokenModifier, "es"}, Token{tokenNumber, "8"}, Token{tokenEOF, ""}}},
 		{"8d6o", []Token{Token{tokenNumber, "8"}, Token{tokenDice, "d"}, Token{tokenNumber, "6"}, Token{tokenModifier, "o"}, Token{tokenEOF, ""}}},
 		{"10d10o", []Token{Token{tokenNumber, "10"}, Token{tokenDice, "d"}, Token{tokenNumber, "10"}, Token{tokenModifier, "o"}, Token{tokenEOF, ""}}},
-		// More complex expressions
+		// More complex expressions (omiting the number of dices -> 1 dice)
 		{"d6o", []Token{Token{tokenDice, "d"}, Token{tokenNumber, "6"}, Token{tokenModifier, "o"}, Token{tokenEOF, ""}}},
 		{"d6e", []Token{Token{tokenDice, "d"}, Token{tokenNumber, "6"}, Token{tokenModifier, "e"}, Token{tokenEOF, ""}}},
 		{"d6es4", []Token{Token{tokenDice, "d"}, Token{tokenNumber, "6"}, Token{tokenModifier, "es"}, Token{tokenNumber, "4"}, Token{tokenEOF, ""}}},
@@ -68,9 +67,17 @@ func TestLexer(t *testing.T) {
 		{"d100k1", []Token{Token{tokenDice, "d"}, Token{tokenNumber, "100"}, Token{tokenModifier, "k"}, Token{tokenNumber, "1"}, Token{tokenEOF, ""}}},
 
 		// Some errors:
-		{" 10000", []Token{Token{tokenError, " 10000"}}},
-		{"10 000", []Token{Token{tokenNumber, "10"}, Token{tokenError, " 000"}}},
+		{" 10000", []Token{Token{tokenError, " 1"}}},
+		{"1000 ", []Token{Token{tokenNumber, "1000"}, Token{tokenError, " "}}},
+		{"10 000", []Token{Token{tokenNumber, "10"}, Token{tokenError, " "}}},
 		{"d6a", []Token{Token{tokenDice, "d"}, Token{tokenNumber, "6"}, Token{tokenError, "a"}}},
+		{"5d6v4", []Token{Token{tokenNumber, "5"}, Token{tokenDice, "d"}, Token{tokenNumber, "6"}, Token{tokenError, "v"}}},
+		// TODO: more test with spaces and invalid dice expressions
+		/*
+			// More complex expressions
+			//{"3d4d5+2", []string{"3", "4", "d5+2"}, 3, 4},
+			//{"2d6d1", []string{"2", "6", "d1"}, 2, 6},   // rolls two six-sided dice, drops the lowest roll, and sums the total
+		*/
 	}
 	var resultTokens = []Token{}
 	for i, lts := range lexerTestStrings {
