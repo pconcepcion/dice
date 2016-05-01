8a99d661d658c51f69f4a6158b5415e03ba420e0
Pablo de la Concepcion <pconcepcion@gmail.com>
2016-05-01 02:59:50 +0200

    Cleaned the code of dice_expressions.go renamed some of the fields of the SimpleDiceExpression struct

--- dice_expressions.go
+++ dice_expressions.go
@@ -3,7 +3,6 @@ package rpg
 
 import (
 	"errors"
-	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
@@ -27,9 +26,9 @@ type SimpleDiceExpression struct {
 	drop           int    // number of lower results to drop
 	keep           int    // number of lower results to keep
 	explodeResult  int    // explode the result if greater than X
-	target         int    // count as success if greater than X
-	rerollL        int    // number of lower dices to reroll
-	rerollH        int    // number of high results to reroll
+	target         int    // target to consider a success the value must be greater than X
+	open           int    // count as success if greater than X
+	reroll         int    // number of high results to reroll
 	constant       int    // constant value
 }
 
@@ -57,7 +56,7 @@ func (sde *SimpleDiceExpression) handleTokenModifier(tok, nextToken Token) {
 			sde.explodeResult = sde.sides
 			sde.target, _ = strconv.Atoi(nextToken.val)
 		case "r":
-			sde.rerollH, _ = strconv.Atoi(nextToken.val)
+			sde.reroll, _ = strconv.Atoi(nextToken.val)
 		default:
 			panic("Unexpected modifier")
 		}
@@ -66,7 +65,7 @@ func (sde *SimpleDiceExpression) handleTokenModifier(tok, nextToken Token) {
 		case "e":
 			sde.explodeResult = sde.sides
 		case "o":
-			sde.explodeResult = sde.sides
+			sde.open = sde.sides
 		}
 	}
 }
@@ -123,82 +122,4 @@ func (sde *SimpleDiceExpression) parse() error {
 		}
 	}
 	return nil
-	/*
-		var err error
-		// Check if it's a constant dice expression
-		if constantRegexp.MatchString(sde.expressionText) == true {
-			sde.constant, err = strconv.Atoi(sde.expressionText)
-			if err != nil {
-				return nil, err
-			}
-			return []string{sde.expressionText, "", ""}, nil
-		}
-		dices := baseRegEx.FindStringSubmatch(strings.TrimSpace(sde.expressionText))
-		if dices == nil {
-			return nil, errors.New("Invalid dice expression")
-		}
-		// If it's not specified assume it's one dice
-		sde.numDices = 1
-		if dices[numDicesPosition] != "" {
-			sde.numDices, err = strconv.Atoi(dices[numDicesPosition])
-			if err != nil {
-				return nil, err
-			}
-		}
-		sde.sides, err = strconv.Atoi(dices[numSidesPosition])
-		if err != nil {
-			return nil, err
-		}
-		if dices[modifierExpression] != "" {
-			modifiers, err := sde.parseModifiers(dices[modifierExpression])
-			if err != nil {
-				return nil, err
-			}
-			dices = append(dices[:len(dices)-1], modifiers[1:]...)
-		}
-		return dices, nil
-	*/
-}
-
-/**
- * parse the modifiers part of the exprssion and store the values on the expression structure
- */
-func (sde *SimpleDiceExpression) parseModifiers(modifierString string) ([]string, error) {
-	var err error
-	modifiers := modifiersRegEx.FindStringSubmatch(modifierString)
-	fmt.Printf("modifiers %s %#v\n", modifierString, modifiers)
-	if modifiers == nil {
-		return nil, errors.New("Invalid dice expression")
-	}
-	switch modifierString[0] {
-	case 'k': // Keep
-		if modifiers[2] == "" {
-			return nil, errors.New("Invalid dice expression, missing number of dices to keep")
-		}
-		sde.keep, err = strconv.Atoi(modifiers[2])
-		if err != nil {
-			return nil, err
-		}
-	case 'r': // Rerroll
-		if modifiers[2] == "" {
-			return nil, errors.New("Invalid dice expression, missing number of dices to reroll")
-		}
-		sde.rerollL, err = strconv.Atoi(modifiers[2])
-		if err != nil {
-			return nil, err
-		}
-	case 's': // Keep
-		if modifiers[2] == "" {
-			return nil, errors.New("Invalid dice expression, missing success number")
-		}
-		sde.target, err = strconv.Atoi(modifiers[2])
-		if err != nil {
-			return nil, err
-		}
-	// case 'd': // Discard
-
-	default:
-		return nil, errors.New("Invalid dice modifier")
-	}
-	return modifiers, nil
 }
