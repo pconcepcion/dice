ba98587cf259d6e199a9680165ebfe8ba354659b
Pablo de la Concepcion <pconcepcion@gmail.com>
2016-05-24 00:48:57 +0200

    Added test for SumTotal

--- dice_expressions_result_test.go
+++ dice_expressions_result_test.go
@@ -142,3 +142,24 @@ func TestSimpleDiceExpressionResultSuccess(t *testing.T) {
 		}
 	}
 }
+
+// TestSimpleDiceExpressionResultSumTotal test
+func TestSimpleDiceExpressionResultSumTotal(t *testing.T) {
+	var simpleDiceExpressionResultTests = []struct {
+		sder simpleDiceExpressionResult
+		out  int
+	}{
+		{simpleDiceExpressionResult{SimpleDiceExpression{}, DiceResults{}, DiceResults{}, 0, false}, 0},
+		{simpleDiceExpressionResult{SimpleDiceExpression{}, DiceResults{7}, DiceResults{}, 0, false}, 7},
+		{simpleDiceExpressionResult{SimpleDiceExpression{}, DiceResults{1, 2, 3, 4, 5, 6, 7}, DiceResults{}, 0, false}, 28},
+		{simpleDiceExpressionResult{SimpleDiceExpression{}, DiceResults{1, 2, 3, 4, 5, 6, 7}, DiceResults{1, 2, 3}, 0, false}, 28},
+	}
+	for i, sdert := range simpleDiceExpressionResultTests {
+		sdert.sder.SumTotal()
+		if sdert.sder.total != sdert.out {
+			t.Errorf("%d) expression: %v expected  %d: got %d", i, sdert.sder, sdert.out, sdert.sder.total)
+		} else {
+			t.Logf("%d) expression: %v Sum Total %d: OK", i, sdert.sder, sdert.sder.total)
+		}
+	}
+}
